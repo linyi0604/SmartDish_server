eb567b1f44a6ddcd63b2a530f617a51e57d15127
linyi0604 <lin_yi_0604@foxmail.com>
2018-03-27 08:45:14 +0800

    20180327-2

--- SmartDish/utils/dishes_sipider.py
+++ SmartDish/utils/dishes_sipider.py
@@ -1,2 +1,15 @@
 from requests import Request
 
+class DishSpider(object):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    spider = DishSpider()
+    spider.run()
